191f3e74a216ac0540f98ab1ee4e498d3c353662
theskid22 <shrey150436@gmail.com>
2024-06-29 13:58:25 -0400

    Added bare server

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/service/point/blank/',
+	bare: 'bare.electrial.site',
+    prefix: '/service/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv.handler.js',
