4c75cb506ec7a1d49abe73ecc438bba75c97b693
Nobodycares-lo <140128868+Nobodycares-lo@users.noreply.github.com>
2024-06-29 14:31:17 -0600

    Updated Bare

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/service/point/blank/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
