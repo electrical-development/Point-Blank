65f5da88caaff528430be89ebd14a3870c0988bd
Nobodycares-lo <140128868+Nobodycares-lo@users.noreply.github.com>
2024-06-29 20:12:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ server.on("upgrade", (req, socket, head) => {
 
 let port = parseInt(process.env.PORT || "");
 
-if (isNaN(port)) port = 8080;
+if (isNaN(port)) port = 5505;
 
 server.on("listening", () => {
   const address = server.address();
