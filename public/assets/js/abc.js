7a26ab551d3de72887bcc815045b8a9d126d7c7f
sexmastercc <oscarlin@edu.schools.us.kg>
2024-07-01 23:06:46 -0400

    added about blank cloaking

---
+++ public/assets/js/abc.js
@@ -0,0 +1,27 @@
+function toggleAutoCloak() {
+    var autoCloakCheckbox = document.getElementById("autoCloakCheckbox");
+    var win = window.open();
+    if (autoCloakCheckbox.checked) {
+        localStorage.setItem('autoCloakChecked', true); 
+        win.document.body.style.margin = '0';
+        win.document.body.style.height = '100vh';
+        var iframe = win.document.createElement('iframe');
+        iframe.style.border = 'none';
+        iframe.style.width = '100%';
+        iframe.style.height = '100%';
+        iframe.style.margin = '0';
+        iframe.src = 'index.html';
+        win.document.body.appendChild(iframe);
+    } else {
+        localStorage.removeItem('autoCloakChecked');
+        win.close();
+    }
+}
+
+window.onload = function() {
+    var autoCloakCheckbox = document.getElementById("autoCloakCheckbox");
+    var savedState = localStorage.getItem('autoCloakChecked');
+    if (savedState === 'true') {
+        autoCloakCheckbox.checked = true;
+    }
+};
