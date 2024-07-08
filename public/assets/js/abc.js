e074f57432548ef3c3db9838fe7e772cdd318abd
sexmastercc <oscarlin@edu.schools.us.kg>
2024-07-07 21:13:10 -0400

    i hate life

--- public/assets/js/abc.js
+++ public/assets/js/abc.js
@@ -1,28 +1,23 @@
 function toggleAutoCloak() {
     var autoCloakCheckbox = document.getElementById("autoCloakCheckbox");
-    var win = window.open();
-    if (autoCloakCheckbox.checked) {
-        localStorage.setItem('autoCloakEnabled', true); 
-        win.document.body.style.margin = '0';
-        win.document.body.style.height = '100vh';
-        var iframe = win.document.createElement('iframe');
-        iframe.style.border = 'none';
-        iframe.style.width = '100%';
-        iframe.style.height = '100%';
-        iframe.style.margin = '0';
-        iframe.src = 'index.html'; 
-        win.document.body.appendChild(iframe);
+    if (localStorage.getItem('autoCloakEnabled') === 'true') {
+        var win = window.open();
+        if (autoCloakCheckbox.checked) {
+            localStorage.setItem('autoCloakEnabled', true); 
+            win.document.body.style.margin = '0';
+            win.document.body.style.height = '100vh';
+            var iframe = win.document.createElement('iframe');
+            iframe.style.border = 'none';
+            iframe.style.width = '100%';
+            iframe.style.height = '100%';
+            iframe.style.margin = '0';
+            iframe.src = 'abc.html'; 
+            win.document.body.appendChild(iframe);
+        } else {
+            localStorage.removeItem('autoCloakEnabled');
+            win.close();
+        }
     } else {
-        localStorage.removeItem('autoCloakEnabled');
-        win.close();
+        return;
     }
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    var autoCloakCheckbox = document.getElementById("autoCloakCheckbox");
-    autoCloakCheckbox.addEventListener('change', toggleAutoCloak);
-
-    if (localStorage.getItem('autoCloakEnabled') === 'true') {
-        toggleAutoCloak();
-    }
-});
